4f9902a73d337721eb5c81b09562bbc1e7d2b792
zey <36336775+zeyoner@users.noreply.github.com>
2020-12-15 03:26:11 -0500

    Update bootanimation-cp.sh

--- bootanimation-cp.sh
+++ bootanimation-cp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BOOT_ANIMATION="https://anonfiles.com/RcH5jcz2p1/bootanimation_zip"
+BOOT_ANIMATION="https://cdn-114.anonfiles.com/RcH5jcz2p1/3191c8ce-1608021215/bootanimation.zip"
 mkdir -p $BUILD_DIR/branding
 echo "Downloading boot animation $BOOT_ANIMATION"
 wget -O $BUILD_DIR/branding/bootanimation.zip $BOOT_ANIMATION
